6aaa0a400866a08edb234c394afba49e43f2a89b
JohnL <johnwl.11235@gmail.com>
2016-10-12 01:13:56 +0000

    Added generic rotate method to star, can also be used for blinking

--- elements.py
+++ elements.py
@@ -52,5 +52,72 @@ class led(object):
 		    break
 	GPIO.output(self.pin, end_state)
 
+class star(object):
+
+    def __init__(self, pin_order=None):
+        if pin_order is None:
+            self.pin = (36,37,32,33,31,29,22,18,16,15)
+        else:
+            self.pin = pin_order
+        self.N = len(self.pin)
+
+        for pn in self.pin:
+            GPIO.setup(pn, GPIO.OUT)
+
+    def engine(self, seq):
+        for d in seq:
+            if d[1] != []:
+                GPIO.output([self.pin[p] for p in d[1]], 1)
+            if d[2] != []:
+                GPIO.output([self.pin[p] for p in d[2]], 0)
+            time.sleep(d[0])
+
+    def on(self):
+        GPIO.output(self.pin, 1)
+
+    def off(self):
+        GPIO.output(self.pin, 0)
+
+    def rotate(self, carrier=[0], v=1, s=1, b='on', dt=1, Nsteps=10, run=True):
+	"""
+	carrier = initial strip position(s) to 'move'
+	v = direction of rotation (1 or -1)
+	s = step size of rotation
+	dt = time between steps
+	Nsteps = number of steps
+	b = carrier state (on or off)
+	"""
+
+	if v > 0:
+	    v = 1
+	elif v < 0:
+	    v = -1
+
+	seq = []
+
+	C = carrier
+	C_ = [n for n in range(self.N) if n not in C]
+	if b is 'off':
+	    temp = C
+	    C = C_
+	    C_ = temp
+	seq.append((dt, C, C_))
+
+	def shift(plist):
+	    return [((p + v*s) % self.N) for p in plist]
+
+        for rev in range(Nsteps):
+	    C = shift(C)
+	    C_ = shift(C_)
+	    seq.append((dt, C, C_))
+	    print rev
+	    print C
+	    print C_
+	
+	if run:
+            self.engine(seq)
+	else:
+	    return seq
+ 
 if __name__ == '__main__':
     pass
